2517e3b60b0d4839b36f833b87fc34359e4b49d0
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-14 00:12:31 -0500

    Test boards set up

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -4,15 +4,115 @@
 // It assumes two players on the same key board
 // It must display the state of the board on the console
 
-// Plans
+// Creates New MainGame Object
+
+//Test area
+// User Selection menu
+// User loop to set up numbers
+string CurrentPlayer = "Player 1";
+Console.WriteLine("***********************************");
+Console.WriteLine($"{CurrentPlayer}'s Turn");
+Console.WriteLine("Select one of the numbers below:");
+Console.WriteLine("***********************************");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| 1 | 2 | 3 |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| 4 | 5 | 6 |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| 7 | 8 | 9 |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine("***********************************");
+Console.WriteLine("\n");
+
+//Prototype board
+char[,] board = new char[3, 3];
+board[0, 0] = 'X';
+Console.WriteLine("***********************************");
+Console.WriteLine("Sample board");
+Console.WriteLine("***********************************");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| {board[0, 0]} | O | X |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| O | X | X |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| X | O | O |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine("***********************************");
+
+//array test 1
+char[,] board2 = new char[3, 3] { { '1','2','3'}, { '4','5','6' }, { '7','8','9' } };
+Console.WriteLine("***********************************");
+Console.WriteLine("Sample Array test board");
+Console.WriteLine("***********************************");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| {board2[0, 0]} | {board2[0, 1]} | {board2[0, 2]} |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| {board2[1, 0]} | {board2[1, 1]} | {board2[1, 2]} |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| {board2[2, 0]} | {board2[2, 1]} | {board2[2, 2]} |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine("***********************************");
+
+
+//array test 2
+char[,] board3 = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+
+board3[0, 0] = 'X';
+Console.WriteLine("***********************************");
+Console.WriteLine("Sample Array test board");
+Console.WriteLine("***********************************");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| {board3[0, 0]} | {board3[0, 1]} | {board3[0, 2]} |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| {board3[1, 0]} | {board3[1, 1]} | {board3[1, 2]} |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine($"| {board3[2, 0]} | {board3[2, 1]} | {board3[2, 2]} |");
+Console.WriteLine("|---+---+---|");
+Console.WriteLine("***********************************");
+Console.ReadKey();
+
 
 
 public class MainGame
 {
+//Set up
+// Creates New Board Object
+// Ceates New Board Draw Object
+// Creates Player1
+//Creates Player2
+
+//Loop of nine turns 
+// Draw board
+// Next player, to pick spot
+// Checks for win via win Method
+// swap player after end of turn (not sure how to do this) could use if player 1 add one to current player, if player 2 subtract one to current player
+
+//After 9 breaks loop gives win to cat. 
+
 
 }
 
-public class board
+public class Board
 {
+ //builds 3X3 array
+ // keeps track of empty array elements
+ // injects user provided info to new cells
+ // tracks cell status 
+}
+
+public class DrawBoard
+{
+//Draws current board
+
 
 }
+
+public class Player
+{
+    //Get players Name?
+    // Get X or O
+    // Pick square
+}
+
+
+
