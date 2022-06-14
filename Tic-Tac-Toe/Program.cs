e30327fb0ebb76a78c9e915d9f21ddd7a3e236af
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-13 22:24:04 -0500

    Add project files.

---
+++ Tic-Tac-Toe/Program.cs
@@ -0,0 +1,5 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 24 "Tic-Tac-Toe" Challenge
+// The task is create a Tic-Tac-Toe game
+// It assumes two players on the same key board
+// It must display the state of the board on the console
