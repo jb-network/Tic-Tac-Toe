a0751eca45c7c97befabdabb84983d55b6705f1f
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-16 01:31:46 -0500

    Update Program.cs

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -1,233 +1,233 @@
-﻿// This is challenge work for the "C# Players Guide"
+// This is challenge work for the "C# Players Guide"
 // Level 24 "Tic-Tac-Toe" Challenge
 // The task is create a Tic-Tac-Toe game
 // It assumes two players on the same key board
 // It must display the state of the board on the console
 
-// Creates New MainGame Object
+new Game().NewGame();
 
-//Test area
-// User Selection menu
-// User loop to set up numbers
-
-
-//This is a rough prototype of the game loop.  More is neede but the basics are here. 
-// A cheap trick for player rotation could be to use round with Mod to get even or odd round number.  If odd player 1 turn, if even player 2 turn. There might be an better way.  
-Console.WriteLine("Pick the current player for testing: 'Player1' or 'Player2'");
-string CurrentPlayer = Console.ReadLine();
-char CurrentLetter;
-//Sets current letter based on player
-if (CurrentPlayer == "Player1") CurrentLetter = 'X';
-else CurrentLetter = 'O';
-
-//Builds 3x3 array
-char[,] board4 = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
-
-//Sample set up menu at start of game
-Console.WriteLine("***********************************");
-Console.WriteLine($"{CurrentPlayer}'s Turn");
-Console.WriteLine("***********************************");
-Console.WriteLine("Sample Array test board");
-Console.WriteLine("***********************************");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine($"| {board4[0, 0]} | {board4[0, 1]} | {board4[0, 2]} |");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine($"| {board4[1, 0]} | {board4[1, 1]} | {board4[1, 2]} |");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine($"| {board4[2, 0]} | {board4[2, 1]} | {board4[2, 2]} |");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine("***********************************");
-
-//Sample user input
-Console.WriteLine("Select one of the numbers:");
-char UserSelection = Convert.ToChar(Console.Read());
-Console.Clear();
-
-//Test switch to update board
-switch (UserSelection)
+public class Game
 {
-    case '1': 
-        board4[0, 0] = CurrentLetter;
-        break;
-    case '2':
-        board4[0, 1] = CurrentLetter;
-        break;
-    case '3':
-        board4[0, 2] = CurrentLetter;
-        break;
-    case '4':
-        board4[1, 0] = CurrentLetter;
-        break;
-    case '5':
-        board4[1, 1] = CurrentLetter;
-        break;
-    case '6':
-        board4[1, 2] = CurrentLetter;
-        break;
-    case '7':
-        board4[2, 0] = CurrentLetter;
-        break;
-    case '8':
-        board4[2, 1] = CurrentLetter;
-        break;
-    case '9':
-        board4[2, 2] = CurrentLetter;
-        break;
-    default:
-        Console.WriteLine("Not a valid selection");
-        break;
-        
-}
-//Test board after update. Updates to 0,2 or number 3 will trigger test win
-Console.WriteLine("***********************************");
-Console.WriteLine($"{CurrentPlayer}'s Turn");
-Console.WriteLine("***********************************");
-Console.WriteLine("Sample Array test board");
-Console.WriteLine("***********************************");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine($"| {board4[0, 0]} | {board4[0, 1]} | {board4[0, 2]} |");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine($"| {board4[1, 0]} | {board4[1, 1]} | {board4[1, 2]} |");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine($"| {board4[2, 0]} | {board4[2, 1]} | {board4[2, 2]} |");
-Console.WriteLine("|---+---+---|");
-Console.WriteLine("***********************************");
-
-//The test win can be updated with currentletter each round and checked for a winner before moving to the next player. 
-// This should return a bool true or false once set up in a Method, True if won, False if lost.  check each line. 
-//Add across x 3
-if (board4[0, 0] == '1' && board4[0, 1] == '2' && board4[0, 2] == CurrentLetter) Console.WriteLine("Test: WINNER!");
-// Add up x 3
-// Add diagonal x 2
-
-Console.ReadKey();
-
-//Testing Player switching
-int round = 0;
-Console.WriteLine("Set round for testing, Round number: ");
-round = Convert.ToInt32(Console.ReadLine());
-if ( round % 2 != 0) CurrentPlayer = "Player1";
-else CurrentPlayer = "Player2";
-Console.Write($"Current Player: {CurrentPlayer}");
-Console.ReadKey();
-
-//Testing Loop to check for X and O
-/*
-int trackertest = 0;
-do
-{
-    //Sample set up menu at start of game
-    Console.WriteLine("***********************************");
-    Console.WriteLine($"{CurrentPlayer}'s Turn");
-    Console.WriteLine("***********************************");
-    Console.WriteLine("Sample Array test board");
-    Console.WriteLine("***********************************");
-    Console.WriteLine("|---+---+---|");
-    Console.WriteLine($"| {board4[0, 0]} | {board4[0, 1]} | {board4[0, 2]} |");
-    Console.WriteLine("|---+---+---|");
-    Console.WriteLine($"| {board4[1, 0]} | {board4[1, 1]} | {board4[1, 2]} |");
-    Console.WriteLine("|---+---+---|");
-    Console.WriteLine($"| {board4[2, 0]} | {board4[2, 1]} | {board4[2, 2]} |");
-    Console.WriteLine("|---+---+---|");
-    Console.WriteLine("***********************************");
-
-    //Sample user input
-    Console.WriteLine("Select one of the numbers:");
-    char UserSelection = Convert.ToChar(Console.Read());
-    Console.Clear();
-
-    //Test switch to update board
-    switch (UserSelection)
+    public void NewGame()
     {
-        case '1':
-            if (board4[0, 0] == 'X' || board4[0, 0] == 'Y')
+        //Build Game start
+ 
+        //Build Player object
+        //Player 1
+        int Player1ID = 1;
+        char Player1Symbol = 'X';
+        string Player1Name = Player.GetPlayerNames(Player1ID);
+        Player Player_1 = new Player(Player1Name, Player1ID, Player1Symbol);
+
+        //Player 2
+        int Player2ID = 2;
+        char Player2Symbol = 'O';
+        string Player2Name = Player.GetPlayerNames(Player2ID);
+        Player Player_2 = new Player(Player2Name, Player2ID, Player2Symbol);
+
+        //string GameRoundPlayer = Player_1.UserName;
+        //Create Board
+        Board Gameboard = new();
+
+        //Game Play loop
+        int GameRound = 1;
+        
+        while (GameRound < 10)
+        {
+            //Sets current player
+            string GameRoundPlayer = Player.SetRound(GameRound, Player_1.UserName, Player_2.UserName);  //Selects the correct player for the round
+
+            //Sets current player symbol
+            char GameRoundSymbol;
+            if (GameRoundPlayer == Player_1.UserName) GameRoundSymbol = Player_1.Symbol;
+            else GameRoundSymbol = Player_2.Symbol;
+
+            //Shows the board before the current player takes an action
+            Board.ShowBoardBeforeMove(Gameboard, GameRound, GameRoundPlayer); 
+            
+            //Allows the player to pick location for Sumbol
+            Board.PlayerChoice(GameRoundPlayer, GameRoundSymbol, Gameboard);
+            
+            //Shows the board after the current player takes an action
+            Board.ShowBoardAfterMove(Gameboard, GameRound, GameRoundPlayer);
+            
+            //Check for 3 in a row
+            bool CurrentWin = WinCheck(Gameboard, GameRound, GameRoundPlayer, GameRoundSymbol) ;
+            if (CurrentWin == true)
             {
-                Console.WriteLine("That space is aleady taken");
-                trackertest = 0;
-                break;
+                Console.WriteLine("WINNER, WINNER, CHICKEN DINNER!");
+                Console.WriteLine($"Congratulations {GameRoundPlayer} you won Tic-Tac-Toe!");
+                Console.WriteLine("The game is over");
+                Console.WriteLine("Press any key to end the game");
+                Console.ReadKey();
+                Environment.Exit(0);
             }
+            else if(GameRound == 9)
+            {
+                Console.WriteLine("YOU NOT THE WINNER, BUT YOU ARE NOT THE LOSER");
+                Console.WriteLine("The Game Ended in a Tie");
+                Console.WriteLine("Press any key to end the game");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            
             else
             {
-                board4[0, 0] = CurrentLetter;
-                trackertest = 1;
-                break;
+                Console.WriteLine("Press Any Key to move to the next round");
+                Console.ReadKey();
+                Console.Clear();
+                GameRound++;
             }
-        case '2':
-            board4[0, 1] = CurrentLetter;
-            break;
-        case '3':
-            board4[0, 2] = CurrentLetter;
-            break;
-        case '4':
-            board4[1, 0] = CurrentLetter;
-            break;
-        case '5':
-            board4[1, 1] = CurrentLetter;
-            break;
-        case '6':
-            board4[1, 2] = CurrentLetter;
-            break;
-        case '7':
-            board4[2, 0] = CurrentLetter;
-            break;
-        case '8':
-            board4[2, 1] = CurrentLetter;
-            break;
-        case '9':
-            board4[2, 2] = CurrentLetter;
-            break;
-        default:
-            Console.WriteLine("Not a valid selection");
-            trackertest = 0;
-            break;
+
+        }
+        //End of program
+        Console.ReadKey();
     }
-}
 
-while (trackertest == 0)
-*/
+    private bool WinCheck(Board Gameboard, int Round, string Player, char PlayerSymbol)
+    {
+        //Check Diagonal for win
+        if (Gameboard.BoardStatus[0, 0] == PlayerSymbol && Gameboard.BoardStatus[1, 1] == PlayerSymbol && Gameboard.BoardStatus[2, 2] == PlayerSymbol) return true;
+        if (Gameboard.BoardStatus[2, 0] == PlayerSymbol && Gameboard.BoardStatus[1, 1] == PlayerSymbol && Gameboard.BoardStatus[0, 2] == PlayerSymbol) return true;
 
-public class MainGame
-{
-//Set up
-// Creates New Board Object
-// Ceates New Board Draw Object
-// Creates Player1
-//Creates Player2
+        // Check Rows for win
+        if (Gameboard.BoardStatus[0, 0] == PlayerSymbol && Gameboard.BoardStatus[0, 1] == PlayerSymbol && Gameboard.BoardStatus[0, 2] == PlayerSymbol) return true;
+        if (Gameboard.BoardStatus[1, 0] == PlayerSymbol && Gameboard.BoardStatus[1, 1] == PlayerSymbol && Gameboard.BoardStatus[1, 2] == PlayerSymbol) return true;
+        if (Gameboard.BoardStatus[2, 0] == PlayerSymbol && Gameboard.BoardStatus[2, 1] == PlayerSymbol && Gameboard.BoardStatus[2, 2] == PlayerSymbol) return true;
 
-//Loop of nine turns 
-// Draw board
-// Next player, to pick spot
-// Checks for win via win Method
-// swap player after end of turn (not sure how to do this) could use if player 1 add one to current player, if player 2 subtract one to current player
-
-//After 9 breaks loop gives win to cat. 
-
-
-
-
+        // Check Columns for win
+        if (Gameboard.BoardStatus[0, 0] == PlayerSymbol && Gameboard.BoardStatus[1, 0] == PlayerSymbol && Gameboard.BoardStatus[2, 0] == PlayerSymbol) return true;
+        if (Gameboard.BoardStatus[0, 1] == PlayerSymbol && Gameboard.BoardStatus[1, 1] == PlayerSymbol && Gameboard.BoardStatus[2, 1] == PlayerSymbol) return true;
+        if (Gameboard.BoardStatus[0, 2] == PlayerSymbol && Gameboard.BoardStatus[1, 2] == PlayerSymbol && Gameboard.BoardStatus[2, 2] == PlayerSymbol) return true;
+        return false;
+    }
 }
 
 public class Board
 {
- //builds 3X3 array
- // keeps track of empty array elements
- // injects user provided info to new cells
- // tracks cell status 
-}
+    public char[,] BoardStatus = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
 
-public class DrawBoard
-{
-//Draws current board
+    internal static void ShowBoardBeforeMove(Board Gameboard, int Round, string Player)
+    {
+        Console.WriteLine("***********************************");
+        Console.WriteLine($"ROUND: {Round} PLAYER: {Player}");
+        Console.WriteLine("***********************************");
+        Console.WriteLine($"START OF ROUND {Round}");
+        Console.WriteLine("***********************************");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine($"| {Gameboard.BoardStatus[0, 0]} | {Gameboard.BoardStatus[0, 1]} | {Gameboard.BoardStatus[0, 2]} |");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine($"| {Gameboard.BoardStatus[1, 0]} | {Gameboard.BoardStatus[1, 1]} | {Gameboard.BoardStatus[1, 2]} |");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine($"| {Gameboard.BoardStatus[2, 0]} | {Gameboard.BoardStatus[2, 1]} | {Gameboard.BoardStatus[2, 2]} |");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine("***********************************");
+    }
 
+    internal static void ShowBoardAfterMove(Board Gameboard, int Round, string Player)
+    {
+        Console.Clear();
+        Console.WriteLine("***********************************");
+        Console.WriteLine($"ROUND: {Round} PLAYER: {Player}");
+        Console.WriteLine("***********************************");
+        Console.WriteLine($"END OF ROUND {Round}");
+        Console.WriteLine("***********************************");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine($"| {Gameboard.BoardStatus[0, 0]} | {Gameboard.BoardStatus[0, 1]} | {Gameboard.BoardStatus[0, 2]} |");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine($"| {Gameboard.BoardStatus[1, 0]} | {Gameboard.BoardStatus[1, 1]} | {Gameboard.BoardStatus[1, 2]} |");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine($"| {Gameboard.BoardStatus[2, 0]} | {Gameboard.BoardStatus[2, 1]} | {Gameboard.BoardStatus[2, 2]} |");
+        Console.WriteLine("|---+---+---|");
+        Console.WriteLine("***********************************");
+    }
 
+    internal static void PlayerChoice(string PlayerName, char Symbol, Board Board)
+    {
+        int? UserSelection;
+        do
+        {
+            //Will not work if user enters a string rather than a number
+            //bool isNumber = int.TryParse(s1, out int n); would test for this, but this is beyond the scope of this task
+
+            Console.WriteLine($"Hello {PlayerName}.");
+            Console.WriteLine($"Please choose a number to place an {Symbol} in");
+            UserSelection = Convert.ToInt16(Console.ReadLine());
+
+            switch (UserSelection)
+            {
+                case 1:
+                    Board.BoardStatus[0, 0] = Symbol;
+                    break;
+                case 2:
+                    Board.BoardStatus[0, 1] = Symbol;
+                    break;
+                case 3:
+                    Board.BoardStatus[0, 2] = Symbol;
+                    break;
+                case 4:
+                    Board.BoardStatus[1, 0] = Symbol;
+                    break;
+                case 5:
+                    Board.BoardStatus[1, 1] = Symbol;
+                    break;
+                case 6:
+                    Board.BoardStatus[1, 2] = Symbol;
+                    break;
+                case 7:
+                    Board.BoardStatus[2, 0] = Symbol;
+                    break;
+                case 8:
+                    Board.BoardStatus[2, 1] = Symbol;
+                    break;
+                case 9:
+                    Board.BoardStatus[2, 2] = Symbol;
+                    break;
+                default:
+                    Console.WriteLine("The slection you made is not valid");
+                    Console.WriteLine("Please try again");                                        
+                    break;
+            }
+        } while (UserSelection == null  || UserSelection < 0 || UserSelection > 9 );
+    }
 }
 
 public class Player
 {
-    //Get players Name?
-    // Get X or O
-    // Pick square
+    public string UserName { get; }
+    public int PlayerNumber { get; }
+    public char Symbol { get; }
+
+    public Player(string User, int PlayerNum, char PlayerSymbol)
+    {
+        UserName = User;
+        PlayerNumber = PlayerNum;
+        Symbol = PlayerSymbol;
+    }
+
+    //User provided player names
+    internal static string GetPlayerNames(int PlayerNumber)
+    {
+        Console.WriteLine("Welcome to Tic-Tac-Toe");
+        Console.WriteLine("Best of luck to you!\n");
+        Console.WriteLine($"Please enter Player {PlayerNumber}'s Name: ");
+        string? Name = Console.ReadLine();
+        if (Name == "" || Name == null) Name = $"Player {PlayerNumber}";
+        Console.WriteLine($"Name: {Name} set for Player: {PlayerNumber} ");
+        Console.WriteLine("Press any key to proceed");
+        Console.ReadKey();
+        Console.Clear();
+        return Name;
+    }
+    
+    //Sets the player Name for the Round
+    internal static string SetRound(int GameRound, string Player1, string Player2)
+    {
+        if (GameRound != 1)
+        {
+            if (GameRound % 2 != 0) return Player1;
+            else return Player2;
+        }
+        else return Player1;
+    }
 }
-
-
-
